d393a1da5c65c72d08cc292c3ed53894dd0ec456
Romain Loiseau <romain.loiseau@polytechnique.edu>
2018-01-09 14:51:55 +0100

    first .py

--- alignements_structuraux/alignements.py
+++ alignements_structuraux/alignements.py
@@ -10,17 +10,31 @@ from Bio.Seq import Seq
 import Bio.Alphabet
 
 seq1 = Seq("AGCATTTGGCTGGAAGCG")
+
 print(seq1)
 print(seq1.alphabet)
 print(len(seq1))
+
 seq2 = Seq("AGATGACTACCCTGGGTT")
 
 def longer_common_sequence(seq1, seq2):
     m = np.zeros((len(seq1), len(seq2)))
     m[0, 0] = int(seq1[0] == seq2[0])
+    
+    for i in range(0, len(seq1)-1):
+        m[i+1, 0] = max(m[i, 0], m[i, 0] + int(seq1[i+1] == seq2[0]))
+        
+    for j in range(0, len(seq2)-1):
+        m[0, j+1] = max(m[0, j], m[0, j] + int(seq1[0] == seq2[j+1]))
+            
     for i in range(0, len(seq1)-1):
         for j in range(0, len(seq2)-1):
             m[i+1, j+1] = max(m[i+1, j], m[i, j+1], m[i, j] + int(seq1[i+1] == seq2[j+1]))
-    return(m)
+            
+    return(int(m[len(seq1) - 1, len(seq2) - 1]))
     
 print(longer_common_sequence(seq1, seq2))
+
+def longer_common_sequence(Seqs):
+    print(len(Seqs))
+    dims = 
