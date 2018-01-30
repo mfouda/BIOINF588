feed29db2f9fd8f5a4c7afb2ae4ccd0f61b3822b
Romain Loiseau <romain.loiseau@polytechnique.edu>
2018-01-30 14:58:29 +0100

    from fasta to pickle

---
+++ p_loop/fromPickleArnToFastaProtein.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Jan 30 14:43:39 2018
+
+@author: romai
+"""
+
+from Bio import SeqIO
+import pickle as pk
+
+dicoARN = pk.load(open( "dicoARN.p", "rb" ))
+
