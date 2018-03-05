2777ae9aa85f21cdc04ad6ec163af6ef77e1afe0
basile6 <basile.confavreux@gmail.com>
2018-03-05 14:32:32 +0100

    ajout parser bloc -> msf

--- alignements_structuraux/V_Multiple_InfoStructurale/FromBlocToMsf.py
+++ alignements_structuraux/V_Multiple_InfoStructurale/FromBlocToMsf.py
@@ -1,4 +1,5 @@
 from blocs import*
+import math
 
 def convertBlocMsf(myBloc,name):
     n=myBloc.getNbSeqs
@@ -6,24 +7,32 @@ def convertBlocMsf(myBloc,name):
     nom= name+".txt"
     seqNames=myBloc.getNames()
     f = open(nom,'w')
+    
+    ##entête
     f.write('PileUp /n/n/n/n')
     for i in range(l):
         f.write('Name: ' + seqNames[i] + ' Len: ' + l + ' Check: ' + 4681 + ' Weight:  10.0 /n')
     f.write('/n // /n /n /n /n')
-    nbreLigneMsf= maths.roof(l/50)
+    
+    ##écriture de séquence proprement dite
+    nbreLigneMsf= math.ceil(l/50) 
     for i in range(nbreLigneMsf):
         for j in range(n):
             f.write(seqNames[j]+ '      ')
-            k=i*50
+            k=i*50 #écrire par bloc de 50
+            compteur=0 #ajouter un espace tous les dix caractères
             while ((k<(i+1)*50) and k<l):
-                if myBloc.getSeq[j][k]=="-" or myBloc.getSeq[j][k]=="+": ##tout les 10 espace
+                if myBloc.getSeq[j][k]=="-" or myBloc.getSeq[j][k]=="+": #remplacer les gaps par .
                     f.write('.')
                 else:
-                    f.write()
+                    f.write(myBloc.getSeq[j][k])
+                compteur+=1
+                if (compteur==9):
+                    compteur=0
+                    f.write(' ')
                 k=k+1
                 
     
     f.close()
     
 
-'
